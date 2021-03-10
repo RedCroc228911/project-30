208a769c5188a27d5d4e7f25d40f492c20124360
RedCroc228911 <72772949+RedCroc228911@users.noreply.github.com>
2021-03-09 20:35:00 -0500

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,30 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visiblity = 255;
+        World.add(world, this.body);
+      }
+      display(){
+        if(this.body.speed < 10){
+          var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+        } else {
+          World.remove (world,this.body)
+          push()
+          this.visibilty = this.visibility - 2
+          pop()
+        }
+      }
+}
